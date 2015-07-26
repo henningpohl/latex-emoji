1343c2e696d7f4242c779d5c89ac5ec7e0a9f7f5
Henning Pohl <HenningPohl@gmail.com>
2015-07-26 19:03:25 +0200

    Some refactoring, making things work for pdftex, luatex, and xetex (some features restricted to a subset of those), and making image emoji copyable (bit of a hack).

--- generate_emoji_sty.py
+++ generate_emoji_sty.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from mako.template import Template
 import base64
 import os
 import json
@@ -8,6 +9,8 @@ import requests_cache
 # http://www.unicode.org/emoji/charts/index.html
 # http://www.unicode.org/emoji/charts/full-emoji-list.html
 PAGE_URL = 'http://www.unicode.org/emoji/charts/full-emoji-list.html'
+
+template = Template(filename='emoji.sty.TEMPLATE', output_encoding='utf8')
 
 def get_header_names(header):
     cols = header.find_all('th')
@@ -44,96 +47,6 @@ def save_image(folder, imgSrc, filename):
         with open(filename, 'wb') as out:
             out.write(img)
 
-preamble = r"""\NeedsTeXFormat{LaTeX2e}
-\ProvidesPackage{emoji}
-\RequirePackage{graphicx}
-\RequirePackage{kvoptions}
-\RequirePackage{ifthen}
-\RequirePackage{ifxetex}
-
-\SetupKeyvalOptions{family=emoji, prefix=emoji@}
-
-\newcommand{\@emojifolder}{ios}
-\DeclareVoidOption{ios}{
-    \renewcommand{\@emojifolder}{ios}
-}
-\DeclareVoidOption{android}{
-    \renewcommand{\@emojifolder}{android}
-}
-\DeclareVoidOption{twitter}{
-    \renewcommand{\@emojifolder}{twitter}
-}
-\DeclareVoidOption{windows}{
-    \renewcommand{\@emojifolder}{windows}
-}
-\DeclareVoidOption{bw}{
-    \renewcommand{\@emojifolder}{bw}
-}
-\DeclareVoidOption{text}{
-    \renewcommand{\@emojifolder}{text}
-}
-\DeclareStringOption{font}
-
-\ProcessKeyvalOptions*
-
-\newenvironment{ios-emojis}{%
-  \let\@oldemojifolder\@emojifolder%
-  \renewcommand{\@emojifolder}{ios}%
-}{%
-  \renewcommand{\@emojifolder}{\@oldemojifolder}%
-}
-
-\newenvironment{android-emojis}{%
-  \let\@oldemojifolder\@emojifolder%
-  \renewcommand{\@emojifolder}{android}%
-}{%
-  \renewcommand{\@emojifolder}{\@oldemojifolder}%
-}
-
-\newenvironment{twitter-emojis}{%
-  \let\@oldemojifolder\@emojifolder%
-  \renewcommand{\@emojifolder}{twitter}%
-}{%
-  \renewcommand{\@emojifolder}{\@oldemojifolder}%
-}
-
-\newenvironment{windows-emojis}{%
-  \let\@oldemojifolder\@emojifolder%
-  \renewcommand{\@emojifolder}{windows}%
-}{%
-  \renewcommand{\@emojifolder}{\@oldemojifolder}%
-}
-
-\newenvironment{bw-emojis}{%
-  \let\@oldemojifolder\@emojifolder%
-  \renewcommand{\@emojifolder}{bw}%
-}{%
-  \renewcommand{\@emojifolder}{\@oldemojifolder}%
-}
-
-\newenvironment{text-emojis}{%
-  \let\@oldemojifolder\@emojifolder%
-  \renewcommand{\@emojifolder}{text}%
-}{%
-  \renewcommand{\@emojifolder}{\@oldemojifolder}%
-}
-
-\newcommand{\emoji}[2][\@emojifolder]{%
-  \ifthenelse{\equal{#1}{text}}{%
-	\ifxetex%
-	  \ifx\emoji@font\@empty%
-	  \else%
-	    {\fontspec{\emoji@font}\char"#2}%
-	  \fi%
-	\else%
-	\fi%
-  }{%
-    \includegraphics[height=1em]{#1/#2.png}%
-  }%
-}
-
-"""
-
 def scrape():
     soup = BeautifulSoup(requests.get(PAGE_URL).text, "html5lib")
     table = soup('table')[0]
@@ -141,10 +54,8 @@ def scrape():
     header = table.find('tr')
     keys = get_header_names(header)
 
-    with open('emoji.sty', 'w') as out:
-        out.write(preamble)
-
-        out.write("\\ifxetex\n\\else\n")
+    with open('emoji.sty', 'wb') as out:
+        out_codes = []
         for row in header.find_next_siblings('tr'):
             fields = {k:c for k, c in zip(keys, row.find_all('td')) }
             codes = fields['code'].text.replace('U+', '').split(' ')
@@ -156,15 +67,10 @@ def scrape():
             save_image('windows', fields['wind'], filename)
             save_image('bw', fields['b&w'], filename)
 
-            if len(codes) > 1:
-                continue
+            if len(codes) == 1:
+                out_codes.append("".join(codes))
 
-            code = "".join(codes)
-            img = r"\includegraphics[height=1em]{\@emojifolder/%s}" % filename
-            out.write("\\DeclareUnicodeCharacter{%s}{%s}\n" % (code, img))
-
-        out.write("\\fi\n")
-        out.write("\n\\endinput")
+        out.write(template.render(emojis = out_codes))
 
 if __name__ == '__main__':
     requests_cache.install_cache('scrape_cache')
